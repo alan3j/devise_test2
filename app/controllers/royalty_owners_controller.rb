f51fedbc72afa6eacdb8232c3c73e4d39eab2096
Craig J <craig.jantzen@gmail.com>
2013-05-04 10:12:59 -0500

    Relational sort working; RoyaltyOwner belongs to PublishingHouse

--- app/controllers/royalty_owners_controller.rb
+++ app/controllers/royalty_owners_controller.rb
@@ -4,27 +4,25 @@ class RoyaltyOwnersController < ApplicationController
   require 'test2_helpers.rb'
 
   def index
-
+  ###############################################
     @search_fields = RoyaltyOwner.search_fields
 
     index_set_session
     
     @royalty_owners = RoyaltyOwner.search(session[:search_field], 
       session[:search_term], session[:records_pp], params[:page], 
-      session[:sort_order])
-
-# @applications = Application.find(:all, :order => sort_order('created_at'))
+      session[:sort_order], session[:sort_assoc])
 
   end
 
   def show
+  ###############################################
     @royalty_owner = RoyaltyOwner.find params[:id]
   end
 
-  #-- CONTROLLER HELPERS
-  #========================================================
-
+  #-- Maintain session variables
   def index_set_session
+  ###############################################
     #-- Records per page
     if params[:records_pp] && !params[:records_pp].blank? && params[:records_pp].numeric?
       session[:records_pp] = params[:records_pp].to_i
@@ -55,7 +53,50 @@ class RoyaltyOwnersController < ApplicationController
       session[:sort_order] = sort_order 'id'
     end
 
-  end
+    #-- Sort Association
+    if !params[:sort_assoc].nil?
+      session[:sort_assoc] = params[:sort_assoc]
+    end
 
+  end
 end
 
+
+#========================================================
+#-- HELPERS
+#========================================================
+
+  #-- Creates table header links in index.html.haml
+  def sort_link(title, column, options = {}, association = {})
+  ###############################################
+    #-- Conditionally (:unless), don't make the link, I guess
+    condition = options[:unless] if options.has_key?(:unless)
+
+    #-- Toggle sort direction
+    sort_dir = params[:sort_dir] == 'up' ? 'down' : 'up'
+  
+    #-- Display sort order arrow in the header of the sorted column only
+    if session[:sort_order].nil? || session[:sort_order][/^\S+/] == column.to_s
+      visi_span_beg = ""
+      visi_span_end = ""
+    else
+      visi_span_beg = "<span class='hidden'>"
+      visi_span_end = "</span>"
+    end
+  
+    link_to_unless condition, "#{title} #{visi_span_beg}&nbsp;#{arrow}#{visi_span_end}".html_safe,
+      request.parameters.merge( {:sort_order => column, :sort_dir => sort_dir, :sort_assoc => association} )
+  end
+  
+  #-- Return the field and direction for ORDER BY, for example "id ASC"
+  #-- Note that default is required but not used when params[:sort_order] is filled
+  def sort_order(default)
+  ###############################################
+    "#{(params[:sort_order] || default.to_s).gsub(/[\s;'\"]/,'')} #{params[:sort_dir] == 'down' ? 'DESC' : 'ASC'}"
+  end
+
+  #--
+  def arrow
+  ###############################################
+    params[:sort_dir].nil? ? "" : params[:sort_dir]== "down" ? "&#8595;" : "&#8593;"
+  end 
--- app/models/royalty_owner.rb
+++ app/models/royalty_owner.rb
@@ -6,24 +6,28 @@ class RoyaltyOwner < ActiveRecord::Base
 
   attr_accessible :address, :address2, :city, :fax, :first_name, :last_name, :other_name, :phone, :publishing_house_id, :state, :abbreviation, :zip
 
+  #-- Option names available to search form
   def self.search_fields 
     [['Show ALL',''], 
-     ['Publishing House','publishing_house'], 
      ['Type','abbreviation'], ['First Name','first_name'], 
      ['Last Name','last_name'], ['Other Name','other_name'], 
      ['Phone','phone'], ['City','city'], ['State','state'] ]
   end
 
-  def self.search(search_field, search_term, per_page, page, order)
+  #-- Loads @royalty_owners in royalty_owners_controller.rb
+  def self.search(search_field, search_term, per_page, page, order, associate)
 
     if search_field.nil? or search_field == '' or search_term.nil?
-      paginate :per_page => per_page, :page => page, 
-               :order => order
+      paginate :per_page => per_page, :page => page,
+               :order => order,
+               :include => associate
     else
       paginate :per_page => per_page, :page => page,
                :conditions => ["#{search_field} ilike ?", "%#{search_term}%"],
-               :order => order
+               :order => order,
+               :include => associate
     end
-  end
 
+  end
+    
 end
