6ec1641140295da637357d7bc9f8be1fb5d29784
Craig J <craig.jantzen@gmail.com>
2013-05-06 13:07:30 -0500

    Fix RoyaltyownerProdcutMap controller (used generator this time)

---
+++ app/controllers/royaltyowner_product_maps_controller.rb
@@ -0,0 +1,6 @@
+class RoyaltyownerProductMapsController < ApplicationController
+  before_filter :authenticate_user!
+
+  def index
+  end
+end
---
+++ spec/controllers/royaltyowner_product_map_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe RoyaltyownerProductMapController do
+
+end
